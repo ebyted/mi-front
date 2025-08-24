232b812ee97c91beb4a89b7e0b5fdcf8287df034
Edgar Jimenez <ebyted@gmail.com>
2025-08-23 22:19:25 -0700

    feat: implement UserProfileView to retrieve authenticated user details

--- core/views.py
+++ core/views.py
@@ -3,6 +3,23 @@ from .serializers import ProductSerializer, ProductVariantSerializer, ProductWar
 from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+# Vista para el perfil de usuario
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        user = request.user
+        data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'is_active': user.is_active,
+        }
+        return Response(data)
 
 # 1. Búsqueda avanzada de productos
 class PCProductSearchView(APIView):
