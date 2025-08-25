9560d00e0f9f695f5c0f01ccf12785ba4d157831
Edgar Jimenez <ebyted@gmail.com>
2025-08-24 18:52:10 -0700

    feat: implement ProductCenter component with ProductSelect and ProductInventory integration

--- dbackf/src/pages/ProductCenter.jsx
+++ dbackf/src/pages/ProductCenter.jsx
@@ -1,2 +1,22 @@
-// haz que se muesttre el componente ProductInventory
-<ProductInventory selectedProductObj={selectedProduct} />
+import React, { useState } from 'react';
+import ProductSelect from '../components/ProductSelect';
+import ProductInventory from '../components/ProductInventory';
+
+const ProductCenter = () => {
+	const [selectedProduct, setSelectedProduct] = useState(null);
+
+	const handleProductSelect = (product) => {
+		setSelectedProduct(product);
+	};
+
+	return (
+		<div>
+			<ProductSelect onProductSelect={handleProductSelect} />
+			<div className="mt-4">
+				<ProductInventory selectedProductObj={selectedProduct} />
+			</div>
+		</div>
+	);
+};
+
+export default ProductCenter;
